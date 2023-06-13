5d5ab067562528b7c6122c5f2f9885fcc801632b
SVan22447 <101337701+SVan22447@users.noreply.github.com>
2023-06-13 22:22:31 +0300

    Recharge_v1

--- Scipts/charge.cs
+++ Scipts/charge.cs
@@ -1,12 +1,19 @@
 using Godot;
 using System;
 
-public partial class charge : Node2D
-{
-
+public partial class charge : Node2D{
+    CharacterBody2D GG;
+	RomaGay Roma;
+    public override void _Ready()
+    {
+        GG=GetNode<CharacterBody2D>("/root/Test1/CharacterBody2D");
+		Roma = GetNode<RomaGay>("/root/RomaGay");
+    }
     public void ChargeBullet(Node2D body){
 		if(body is CharacterBody2D){
-            GetNode<CharacterBody2D>("/root/Test1/CharacterBody2D").bulletAmount=4;
+            GG.bulletAmount=4;
+			GG.AddJumping= true;
+			Roma.Hilling(1);
 		}
 		GD.Print("Recharge");
 	}
