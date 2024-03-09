f1395ded85cefcdede6a575cef9ff37fedc676e1
SVan22447 <101337701+SVan22447@users.noreply.github.com>
2024-03-09 20:00:32 +0300

    Cool bullets
    YEEEEEEES, THEN WEE HAVE A COOL EFFECTS FOR BULLETS!!!!!!!!!!!!!!!!!!

--- Scripts/InputRemapping.cs
+++ Scripts/InputRemapping.cs
@@ -1,8 +1,10 @@
 using Godot;
 using System;
+using System.Threading;
 
 public partial class InputRemapping : Button{
 	public String action;
+	private int Wait=0;
 	string IconText;
 	public override void _Ready(){
 		action=Name;
@@ -15,16 +17,26 @@ public partial class InputRemapping : Button{
 		Icon = GD.Load<CompressedTexture2D>($"res://addons/ActionIcon/Keyboard/{IconText}.png");
 	}
     private void _OnActionButtonToggled(bool ButtonPressed){
-		SetProcessUnhandledKeyInput(ButtonPressed);
-		if(ButtonPressed){
-			Text= "...";
-		}else{
-			DisplayKey();
-		}
+			// if(Input.IsActionPressed("ui_accept")&&Wait<1){
+			// 	Wait=0;
+			// }else{
+			// 	Wait=1;
+			// }
+			// // if(Input.IsActionJustReleased("ui_accept")){
+			// // 	Wait=1;
+			// // }
+			// if(Wait==1){
+			// 	Wait=0;
+			SetProcessUnhandledKeyInput(ButtonPressed);
+			// }
+			if(ButtonPressed){
+				Text= "...";
+			}else{
+				DisplayKey();
+			}
 	}
     public override void _UnhandledKeyInput(InputEvent @event){
         RemapKey(@event);
-
 		ButtonPressed=false;
     }
 	public void RemapKey(InputEvent @event){
--- Scripts/bullet.cs
+++ Scripts/bullet.cs
@@ -1,29 +1,44 @@
 using Godot;
 using System;
+using System.CodeDom.Compiler;
+using System.Xml.Resolvers;
 
 public partial class bullet : CharacterBody2D{
-	int speed = 45000;
-	int bulletLife = 60;
-	float gravity = -1000; 
+	int speed = 28000;
+	float gravity = 1000;
+	Timer BulletTime;
+	CpuParticles2D particle;
+	Timer ParticleTime; 
 	Vector2 velocity;
 	Vector2 f;
 	public override void _Ready(){
+		particle=GetNode<CpuParticles2D>("CPUParticles2D");
+		BulletTime=GetNode<Timer>("BulletLife");
+		ParticleTime=GetNode<Timer>("ParticleLife");
 	    f = GetGlobalMousePosition()-this.Position;
 	}
 	public override void _Process(double delta){
-		bulletLife--;
-		if (bulletLife<=0){
-			QueueFree();
+		particle.Set("direction",-f);
+		if (!BulletTime.IsStopped()){
+			var deltaf = (float)delta;
+			velocity = Velocity;
+			velocity=f.Normalized()*deltaf*speed;
+			velocity.Y +=  gravity * (float)delta;
+			for (int i = 0; i < GetSlideCollisionCount(); i++){
+				BulletStop();
+				BulletTime.Stop();
+			}
+			Velocity = velocity;
+			MoveAndSlide();
 		}
-		var deltaf = (float)delta;
-		velocity = Velocity;
-		velocity=f.Normalized()*deltaf*speed;
-		velocity.Y +=  gravity * (float)delta;
-		for (int i = 0; i < GetSlideCollisionCount(); i++){
-			QueueFree();
-		}
-		Velocity = velocity;
-		MoveAndSlide();
+	}
+	private void BulletStop(){
+		particle.Emitting=false;
+		GetNode<Sprite2D>("Sprite2D").Visible=false;
+		ParticleTime.Start();
+	}
+	private void ParticleStop(){
+		QueueFree();
 	}
 	public void Boom(){
 	}
